fba6338e51cd7a0827e65bb24a9e90891f77cd10
deniznida <denizunat@hotmail.com>
2015-09-16 13:30:08 -0400

    solution with contributing file and .learn tags

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 class App < Sinatra::Base
+<<<<<<< HEAD
 
   # Define your GET '/' route below and respond with "Hello, World!"
 
 
 end
+=======
+  get '/' do
+    "Hello, World!"
+  end
+end
+>>>>>>> solution with contributing file and .learn tags
